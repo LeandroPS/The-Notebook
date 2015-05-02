f99c188a90d4d9e571448e987a9766bcee961cf1
Leandro Pires de Souza <leandro.pires.souza@gmail.com>
2015-05-02 15:04:17 -0400

    Adding texture and first functions

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,13 +1,70 @@
+if (localStorage.getItem("notebooks") === null) {
+	localStorage.setItem("notebooks", "[]");
+	var notebooks = [];
+}else{
+	var notebooks = JSON.parse(localStorage.getItem("notebooks"));
+}
 
+if (localStorage.getItem("pages") === null) {
+	localStorage.setItem("pages", "[]");
+	var pages = [];
+}else{
+	var pages = JSON.parse(localStorage.getItem("pages"));
+}
+function newID(){
+	
+	return "3";
+}
+
+function getColor(){
+	return "green";
+}
+
+function updateStream(){
+	$("section.collection").html("");
+	$.each(pages, function(index, page){
+		$("section.collection").append("<article>"+page.text+"</article>");	
+		
+	});
+}
+
+
+function createPage(t){
+	pages.push({id: newID(), text: t, date: new Date()});
+	localStorage.setItem("pages", JSON.stringify(pages));
+	updateStream();
+}
+
+function createNotebook(n){
+	notebooks.push({name: n, color: getColor()});
+}
 
 
 $(function(){
+	/*
+	if (localStorage.getItem("Notebooks") === null) {
+  		localStorage.setItem("Notebooks", []);
+	}
+	
+	if (localStorage.getItem("pages") === null) {
+		localStorage.setItem("Notebooks", []);
+	}
+	
+	if (localStorage.getItem("pages") === null) {
+		localStorage.setItem("pages", []);
+	}
+	*/
 	$("button.menu").click(function(){
 		$("nav").toggleClass("expanded");
 	});
 	
-	$("div.new-post").click(function(){
+	$("div.new-page").click(function(){
 		$(this).addClass("expanded");
-		$(this).removeClass("unexpanded");
+	});
+	
+	$("div.new-page button.done").click(function(){
+		createPage($("div.new-page textarea").val());
+		$("div.new-page textarea").val("");
+		$("div.new-page").removeClass("expanded");
 	});
 });
